0fec0a02706ffeabd97bfff1e37c3ba43ce790b2
Jonathan Chiu <jonathanhchiu@gmail.com>
2017-06-06 15:42:21 -0700

    Fixed

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,28 @@
 from flask import Flask
 
-app = Flask("crystal")
-#from app import views
-from app import app
+# Initialize a new flask instance
+app = Flask(__name__)
 
-from app import app
-
-app.route('/')
+# When you declare app.route("/something"), this means that when the user
+# visits localhost:5000/something on google chrome, he will see whatever
+# you are returning in the function you defined (index)
+# Note that eventually you will handle much more than 2 routes, so you will
+# want to move this into a different file.
+@app.route('/')
 @app.route('/index')
-def index() :
+def index():
+	"""
+	This function is responsible for all requests that come into
+	http://127.0.0.1:5000/ or http://127.0.0.1:5000/index.
+	"""
 	user = {'nickname' : 'Jonathan'} #fake user
 	return '''
 <html>
 	<head>
-		<titl>Home Page</ title>
+		<title>Home Page</title>
 	</head>
 	<body>
-		<h1?Hello, ''' + user['nickname'] + '''</h1>
+		<h1>Hello, ''' + user['nickname'] + '''</h1>
 	</body>
 </html>
 '''
--- run.py
+++ run.py
@@ -1,2 +1,10 @@
+# This import statement means: go into the app folder (the first app) and
+# import the variable app (defined in line 4 of __init__.py).
+# Note that python detects and runs all __init__.py files, so you don't have
+# to explicitly call it.
 from app import app
+
+# This line starts the app. Then you can visit 'localhost:5000' or
+# 'http://127.0.0.1:5000/'
+# Press control + c to quit
 app.run(debug=True)
