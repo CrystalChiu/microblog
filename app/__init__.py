f1386e1139745b7f2816fd3fe8c8811558e4a558
Admin <admin@Admins-MacBook-3.local>
2017-06-06 15:23:20 -0700

    work in progress

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,20 @@ from flask import Flask
 app = Flask("crystal")
 #from app import views
 from app import app
-@app.route('/') 
-@app.route('/index')
-def index():
-	return "Hello, World!"
 
+from app import app
+
+app.route('/')
+@app.route('/index')
+def index() :
+	user = {'nickname' : 'Jonathan'} #fake user
+	return '''
+<html>
+	<head>
+		<titl>Home Page</ title>
+	</head>
+	<body>
+		<h1?Hello, ''' + user['nickname'] + '''</h1>
+	</body>
+</html>
+'''
